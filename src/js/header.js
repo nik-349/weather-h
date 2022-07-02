16c2f94d36ad54eee9d876942b5e0a4fe3a322b9
nik-349 <70761612+nik-349@users.noreply.github.com>
2022-07-02 20:58:28 +0300

    style

--- src/js/header.js
+++ src/js/header.js
@@ -1,15 +1,25 @@
 import { refs } from './refs';
 const throttle = require('lodash.throttle');
 const debounce = require('lodash.debounce');
+const Handlebars = require('handlebars');
+import templateWeather from '../template/template.hbs';
 
 const throttledInput = debounce(onChangeInput, 3000);
 refs.loginButton.addEventListener('click', onClickLogin);
 refs.inputSerach.addEventListener('input', throttledInput);
 refs.burgerMenu.addEventListener('click', onClickBurgerMenu);
+refs.searchButton.addEventListener('click', onClickButtonSearch);
+refs.searchButtonIp.addEventListener('click', onClickButtonIp);
+
+function onClickButtonSearch() {
+  refs.inputSerach.classList.toggle('visually-hidden');
+}
 
 function onClickBurgerMenu() {
   refs.backdrop.addEventListener('click', onClickBackdrop);
   refs.burgerMenuList.classList.toggle('visually-hidden');
+  refs.burgerExit.classList.toggle('visually-hidden');
+  refs.burgerIn.classList.toggle('visually-hidden');
 }
 
 function onClickLogin() {
@@ -24,15 +34,32 @@ function onClickBackdrop(event) {
   onClickLogin();
 }
 
+// /astronomy.json - закат восход
+
 const KEY = '94f78073bd334e8ab23192239222706';
 
-function onChangeInput(e) {
-  let country = e.target.value;
-  const URL = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${country}`;
+function onClickButtonIp() {
+  const URL = `http://api.weatherapi.com/v1/ip.json?key=${KEY}&q=auto:ip`;
 
   const fe = fetch(URL)
     .then(response => {
       return response.json();
     })
-    .then(console.log);
+    .then(renderWeather);
+}
+
+function onChangeInput(e) {
+  let country = e.target.value;
+  const URL = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${country}&aqi=yes`;
+
+  const fe = fetch(URL)
+    .then(response => {
+      return response.json();
+    })
+    .then(renderWeather);
+}
+
+function renderWeather(weather) {
+  const markup = templateWeather(weather);
+  refs.moduleDay.innerHTML = markup;
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,7 +2,12 @@ export const refs = {
   singIn: document.querySelector('.sing__in--backdrop'),
   loginButton: document.querySelector('.login__button'),
   backdrop: document.querySelector('.sing__in--backdrop'),
-  inputSerach: document.querySelector('.search__country--input'),
+  inputSerach: document.querySelector('.search__input'),
+  searchButton: document.querySelector('.search__button'),
+  searchButtonIp: document.querySelector('.search__button--ip'),
   burgerMenu: document.querySelector('.burger__menu'),
   burgerMenuList: document.querySelector('.module__burger'),
+  burgerExit: document.querySelector('.burger__exit'),
+  burgerIn: document.querySelector('.burger__in'),
+  moduleDay: document.querySelector('.day'),
 };
